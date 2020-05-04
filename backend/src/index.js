9c0b9758c435473fad8845ece30834c6c5c2b5a6
Léu Almeida <leo@webid.net.br>
2020-05-04 17:14:05 -0300

    feat(app): implementing initial methods

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,31 @@
-const express = require('express');
+const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
+app.use(express.json())
 
-app.get('/projects', (req, res) => {
-  return res.json([
-    'Projeto 1',
-    'Projeto 2'
-  ]);
+const projects = [];
+
+app.get("/projects", (req, res) => {
+  const { title, owner } = req.query;
+
+  return res.json(projects);
 });
 
-app.post('/projects', (req, res) => {
-  return res.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3'
-  ])
-})
+app.post("/projects", (req, res) => {
+  const { title, owner } = req.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+app.put("/projects/:id", (req, res) => {
+  return res.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+});
 
 app.listen(3333, () => {
-  console.log('Back-end started! 👻');
+  console.log("Back-end started! 👻");
 });
