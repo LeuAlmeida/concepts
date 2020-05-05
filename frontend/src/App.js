bd1c6e7d8894954c1a664ed52e78f4432a3cf828
Léu Almeida <leo@webid.net.br>
2020-05-05 16:15:33 -0300

    chore(src/App): updating example content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-import './App.css'
+import "./App.css";
+import backgroundImage from "./assets/background.jpeg";
 
 import Header from "./components/Header";
 
@@ -16,13 +17,18 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={300} src={backgroundImage} />
+
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
         ))}
       </ul>
 
-      <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
     </>
   );
 }
