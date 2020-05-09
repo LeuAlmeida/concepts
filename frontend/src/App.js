d416bef9d87682e112e298bb4349f072c5cd4448
Léu Almeida <leo@webid.net.br>
2020-05-09 15:27:36 -0300

    feat(App): implementing API consuming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./App.css";
-import backgroundImage from "./assets/background.jpeg";
 
 import Header from "./components/Header";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desevolvimento Mobile, Desenvolvimento Front-end",
-  ]);
+  const [projects, setProjects] = useState([]);
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
   }
 
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data)
+    });
+  }, []);
+
   return (
     <>
       <Header title="Projects" />
 
-      <img width={300} src={backgroundImage} />
-
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
