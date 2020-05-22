cbc6ad72cd1bf404e1ee8578b39f3f12e0be9389
Léu Almeida <leo@webid.net.br>
2020-05-22 14:00:28 -0300

    feat(routes): exemplifying use of types

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "leo@webid.net.br",
+    password: "123456",
+    techs: ["ReactJS", "Node.js", { title: "Título", experience: 12 }],
+  });
+
   return response.json({ message: "Hello World" });
 }
